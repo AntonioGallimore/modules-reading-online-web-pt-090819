1b616c560df4ee7c520ea7da3c9c79c7f3b23d30
Antonio Gallimore <antonio.gallimore@gmail.com>
2019-10-20 13:58:01 +0000

    Automatically backed up by Learn

--- lib/kid.rb
+++ lib/kid.rb
@@ -2,16 +2,9 @@ require_relative './dance_module.rb'
 require_relative './class_methods_module.rb'
 
 class Kid
-  include Dance
   
-  class Kid
-  extend MetaDancing
-  end
-  
-  class Kid
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
-  end
  
   attr_accessor :name
  
